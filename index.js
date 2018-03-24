e14a67da7b0f81cdc5c69d86ebacc765b74fc3c2
HyperGalactic <legendmccljr@gmail.com>
2018-03-23 21:51:37 -0300

    .

--- index.js
+++ index.js
@@ -290,6 +290,12 @@ return}
                                     }
                                     if(message.content.startsWith(prefix + "set")) {
                                        
+                                        if(!message.member.roles.find('name', "🌐 Hyper")) {
+                                            message.delete();
+                                            message.reply("apenas o Hyper pode usar este comando. sz");
+                                            return;
+                                        }
+
                                         var args = message.content.split(' ').slice(2).join(' ');
 
                                         if(!args) {
@@ -314,7 +320,7 @@ return}
                                             member.addRole(friend);
                                             message.channel.send(`Successfully. Now ${member} has role ${friend}`)
                                         }
-                                        }
+                                        
 
                                         if(args == "old") {
                                             let old = message.member.guild.roles.find('name', "💛 Old's");
@@ -337,8 +343,76 @@ return}
                                             message.channel.send(`Successfully. Now ${member} has role ${best}`)
                                         }
 
-       
-                                
+
+
+                                }
+
+                                if(message.content.startsWith("!remove")) {
+                                    if(!message.member.roles.find('name', "🌐 Hyper")) {
+                                        message.delete();
+                                        message.reply("apenas o Hyper pode usar este comando. sz");
+                                        return;
+                                    }
+
+                                    var args = message.content.split(' ').slice(2).join(' ');
+
+                                    if(!args) {
+                                        return message.channel.send("Incorrect Arguments, use !remove <user-mention> <newbie,friend,old,best>");
+                                    }
+                                    if(args == "newbie") {
+                                        let newbie = message.member.guild.roles.find('name', "💜 Newbie's");
+                                        let member = message.mentions.members.first();
+                                        if(member.roles.has(newbie.id)) {
+                                            return message.channel.send("This member don't have this role.");
+                                        }
+                                        member.removeRole(newbie);
+                                        message.channel.send(`Successfully. Now ${member} hasn't role ${newbie}`)
+                                    }
+
+                                    if(args == "friend") {
+                                        let friend = message.member.guild.roles.find('name', "💙 Friend's");
+                                        let member = message.mentions.members.first();
+                                        if(member.roles.has(friend.id)) {
+                                            return message.channel.send("This member don't have this role.");
+                                        }
+                                        member.removeRole(friend);
+                                        message.channel.send(`Successfully. Now ${member} hasn't role ${friend}`)
+                                    }
+                                    
+
+                                    if(args == "old") {
+                                        let old = message.member.guild.roles.find('name', "💛 Old's");
+                                        let member = message.mentions.members.first();
+                                        if(!member.roles.has(old.id)) {
+                                            return message.channel.send("This member don't have this role.");
+                                        }
+                                        member.removeRole(old);
+                                        message.channel.send(`Successfully. Now ${member} hasn't role ${old}`)
+                                    }
+                                    
+
+                                    if(args == "best") {
+                                        let best = message.member.guild.roles.find('name', "💚 Best's");
+                                        let member = message.mentions.members.first();
+                                        if(!member.roles.has(best.id)) {
+                                            return message.channel.send("This member don't have this role.");
+                                        }
+                                        member.removeRole(best);
+                                        message.channel.send(`Successfully. Now ${member} hasn't role ${best}`)
+                                    }
+
+                                }
+
+                                if(message.content.startsWith("!nitro")) {
+                                    let nitro = message.member.guild.roles.find('name', "Nitro");
+                                    let member = message.member;
+                                    if(member.roles.has(best.id)) {
+                                        return message.channel.send("You already have this role.");
+                                    }
+                                    member.addRole(nitro);
+                                    message.channel.send(`Successfully. Now ${member} has role ${nitro}`);
+                                }
+
 });
 
 function getValue(key, array) {
