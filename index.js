0e35e833d78247c4fefa0b93ea5306f33e0e943d
HyperGalactic <legendmccljr@gmail.com>
2018-03-25 12:46:17 -0300

    Adding a Clean

--- index.js
+++ index.js
@@ -477,6 +477,13 @@ return}
  
 });
 
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+  }
+
 function getValue(key, array) {
 	for (var el in array) {
 		if (array[el].hasOwnProperty(key)) {
