1c169b2d4accea33c870f5b2c7f225b988311bf6
HyperGalactic <legendmccljr@gmail.com>
2018-03-25 12:43:36 -0300

    Simple eval

--- index.js
+++ index.js
@@ -458,6 +458,22 @@ return}
                                     member.addRole(men);
                                     message.channel.send(`Successfully. Now ${member} has role ${men.name}`);
                                 }
+
+                                if (message.content.startsWith("!eval")) {
+                                    if(message.author.id !== "298139759463890944") return;
+                                    try {
+                                      const code = args.join(" ");
+                                      let evaled = eval(code);
+                                
+                                      if (typeof evaled !== "string")
+                                        evaled = require("util").inspect(evaled);
+                                
+                                      message.channel.send(clean(evaled), {code:"xl"});
+                                    } catch (err) {
+                                      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+                                    }
+                                  }
+                                });
  
  
 });
