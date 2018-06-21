c7035b41ea3f190908c63793e65620e6d775a4b3
HyperGalactic <legendmccljr@gmail.com>
2018-06-21 18:56:17 -0300

    me da vip no central

---
+++ commands/assexuado.js
@@ -0,0 +1,11 @@
+module.exports.run = async(client, message, args) =>{
+
+        let men = message.member.guild.roles.find('name', "Assexuado");
+        let member = message.member;
+        if(member.roles.has(men.id)) {
+            return message.channel.send("You already have this role.");
+        }
+        member.addRole(men);
+        message.channel.send(`Successfully. Now ${member} has role ${men.name}`);
+    
+}
